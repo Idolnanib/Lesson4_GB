5b18ea735b62e6ec80805d599cfedf41d17bccc0
Zhirickii <vovanzhirickii@mail.ru>
2022-12-26 19:22:14 +0300

    Сделано задание на определение трехзначного числа.

--- Lesson_1_basics/Lesson1.py
+++ Lesson_1_basics/Lesson1.py
@@ -8,4 +8,22 @@
 # for num in reversed(my_list):
 #     print(num)
 
-d
+# Задача. Написать программу, определяющую, что число
+# трёхзначное и средняя цифра равна 5.
+
+number = int(input('Введи трехзначное число, а среднюю цифру напиши 5, чтобы по задаче подходило ахах/'
+                   'я то думал надо среднее значение вычислять...мда '))
+if number > 999 or number < 100:
+    print('Число не трехзначное facepalm')
+else:
+    if number // 100 > 0:
+        spare = number // 100 * 100
+        number = number - spare
+        print('количество сотен в числе равно ', spare // 100)
+
+    if number // 10 > 0:
+        spare = number // 10 * 10
+        number = number - spare
+        print('Десятки в числе равны ', spare // 10)
+        print('Количество единиц', number)
+
